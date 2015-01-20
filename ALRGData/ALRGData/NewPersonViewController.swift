dc328c61f2c1bb8873ce9864e284437c3e127620
Paul Rudolph <paul.e.rudolph@gmail.com>
2015-01-20 08:49:46 -0500

    Working on placing some buttons

--- ALRGData/ALRGData/NewPersonViewController.swift
+++ ALRGData/ALRGData/NewPersonViewController.swift
@@ -8,20 +8,96 @@
 
 import UIKit
 import CoreData
-
+import QuartzCore
 
 class NewPersonViewController: UIViewController,UITextFieldDelegate {
-     var managedContex:NSManagedObjectContext?
+   
+    
+    var managedContex:NSManagedObjectContext?
+    var colorButtonsArray = [UIButton]()
+    var selectedColor:String = ""
     
     @IBOutlet weak var firstNamefield: UITextField!
     @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var colorButtonsView: UIView!
 
-    var color:String = ""
+    
+    @IBAction func closePressed(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        addColorButtons()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func addColorButtons(){
+        var autoLayoutDict = Dictionary <String,UIView>()
+        autoLayoutDict["lastView"] = colorButtonsView
+        colorButtonsView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:[lastView(100)]", options: nil, metrics: nil, views: autoLayoutDict))
+        
+        var row = 0
+        var col = 0
+        
+          for  var i = 0;i < 7; ++i {
+        
+            var butt = UIButton()
+            butt.addTarget(self, action: "colorButtonPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+            butt.layer.cornerRadius   = 25
+            butt.backgroundColor = colorDict.values.array[i] as UIColor
+            
+            colorButtonsView.addSubview(butt)
+            butt.setTranslatesAutoresizingMaskIntoConstraints(false)
+            autoLayoutDict["currentButt"] = butt
+            
+            if(i < 4){
+                self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+                    "V:|-5-[currentButt(50)]", options: nil, metrics: nil, views: autoLayoutDict))
+            }else{
+                
+                
+                self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+                    "V:|-55-[currentButt(50)]", options: nil, metrics: nil, views: autoLayoutDict))
+            }
+            
+            if(i == 0){
+                self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+                    "H:|-[currentButt(50)]", options: nil, metrics: nil, views: autoLayoutDict))
+            }else{
+                
+                self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+                    "H:[lastView]-[currentButt(50)]", options: nil, metrics: nil, views: autoLayoutDict))
+                
+            }
+              autoLayoutDict["lastView"] = butt
+           
+        }
+        
+    }
+    
+    func colorButtonPressed(sender: AnyObject){
+        println("ColorButton Pressed\(sender)")
+    
+    }
     
     @IBAction func savePressed(sender: AnyObject) {
-    
         
-       var newperson =  Person(firstName: firstNamefield.text, lastName: lastNameField.text, color: "RED", context:managedContex!)
+        
+        var newperson =  Person(firstName: firstNamefield.text, lastName: lastNameField.text, color: "RED", context:managedContex!)
         
         
         var error: NSError?
@@ -36,43 +112,7 @@ class NewPersonViewController: UIViewController,UITextFieldDelegate {
         
     }
     
-    func addColorButtons(){
-        
-        for var i=0; i < colorDict.count;i++ {
-            
-            var button = UIButton(frame:CGRectMake(10.0 * CGFloat(i), 0, 50, 50))
-            button.backgroundColor = colorDict.values.array[i]
-            self.view.addSubview(button)
     
-        }
-    }
-    
-    
-    @IBAction func closePressed(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-     addColorButtons()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
      func textFieldShouldReturn(textField: UITextField) -> Bool {
             textField.resignFirstResponder()
